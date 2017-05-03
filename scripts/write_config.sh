1fa89999cb382b3777b1d824f7fdfec001fed715
gunjan-lal <gunjan.vk@gmail.com>
2017-05-03 16:47:26 +0530

    rsync

--- scripts/write_config.sh
+++ scripts/write_config.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "extension=php_pdo_mysql.so" >> /etc/php/7.0/fpm/php.ini
-sed "s/'localhost'/'34.202.112.233:3306'/g" /var/www/html/simple-website/libs/constants.php
-sed 's/root/test/g' /var/www/html/simple-website/libs/constants.php
+sed -i "s/'localhost'/'34.202.112.233:3306'/g" /opt/simple-website/libs/constants.php
+sed -i 's/root/test/g' /opt/simple-website/libs/constants.php
+
+rsync -azrh /opt/simple-website/ /var/www/html/
+rsync -azrh /opt/default_nginx_conf/default /etc/nginx/sites-enabled/default
