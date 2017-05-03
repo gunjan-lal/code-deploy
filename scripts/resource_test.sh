437cf94c701a7d689d0316252d34215acbc47483
gunjan-lal <gunjan.vk@gmail.com>
2017-05-03 11:44:27 +0530

    appspec changes

---
+++ scripts/resource_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+services='nginx php-fpm'
+
+for service in $services
+do
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+then
+echo "$service is running!!!"
+else echo "$service is not running"
+fi
+done
+
