aa92e5b683185863faafcec00e3a5d3451669a3d
gunjan-lal <gunjan.vk@gmail.com>
2017-05-03 17:59:06 +0530

    http code

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in `seq 1 10`;
 do
-  HTTP_CODE=`curl --write-out '%{http_code}' -m 10 -q -s http://localhost:80`
+  HTTP_CODE=`curl -s -o /dev/null -w "%{http_code}" http://localhost:80`
   if [ "$HTTP_CODE" == "200" ]; then
     echo "Successfully pulled root page."
     exit 0;
