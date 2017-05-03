29eeed73c8e9030c9a21f2ae83e76092e63016dd
gunjan-lal <gunjan.vk@gmail.com>
2017-05-03 17:33:25 +0530

    rsync check

--- scripts/write_config.sh
+++ scripts/write_config.sh
@@ -3,5 +3,19 @@ echo "extension=php_pdo_mysql.so" >> /etc/php/7.0/fpm/php.ini
 sed -i "s/'localhost'/'34.202.112.233:3306'/g" /opt/simple-website/libs/constants.php
 sed -i 's/root/test/g' /opt/simple-website/libs/constants.php
 
-rsync -azrh /opt/simple-website/ /var/www/html/
-rsync -azrh /opt/default_nginx_conf/default /etc/nginx/sites-enabled/default
+rsync -azvrh /opt/simple-website/ /var/www/html/
+
+if [ $? != 0 ]
+then
+	echo "docroot rync failed"
+	exit 1
+fi
+
+rsync -azrvh /opt/default_nginx_conf/default /etc/nginx/sites-enabled/default
+
+if [ $? != 0 ]
+then
+        echo "nginx default rync failed"
+	exit 1
+fi
+
